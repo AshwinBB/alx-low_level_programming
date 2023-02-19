2f01d20e996230178352a4b2b5dd59c8193f0a15
AshwinBB <ashwinbaatjes@gmail.com>
2023-02-19 15:17:07 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - print numbers between 0 to 9 and letters between a to f.
+ * main - print numbers between 0 to 9 and letters between a to f
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = '0' i <= '9'; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-	for (i = 'a'; i <= 'f'; i++)
+	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 	}
